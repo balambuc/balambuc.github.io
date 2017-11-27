50c82398fbec62256b13d946b1580ec144300dd7
Bence Bárdosi <bardosi.bence@gmail.com>
2017-11-27 12:32:04 +0100

    Finish project

---
+++ js/betumeret.js
@@ -0,0 +1,4 @@
+$( document ).ready(function() {
+    $('#novel').on('click', function() {$("body *").css('font-size','+=1');});
+    $('#csokkent').on('click', function() {$("body *").css('font-size','-=1');});
+});
